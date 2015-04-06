0dca31359fea9e30ecd18dd5c207eb7c115502a6
Joffrey BION <joffrey.bion@gmail.com>
2015-04-06 20:11:39 +0200

    fixed javadoc

--- src/main/java/org/hildan/utils/xml/serialization/serializers/EnumSerializer.java
+++ src/main/java/org/hildan/utils/xml/serialization/serializers/EnumSerializer.java
@@ -4,9 +4,9 @@ import java.text.ParseException;
 
 /**
  * An XML serializer for any enum type, using {@link Enum#toString()} and
- * {@link Enum#valueOf(Class, String)}. If this behaviour is not sufficient,
+ * {@code Enum#valueOf(Class, String)}. If this behaviour is not sufficient,
  * {@link SimpleSerializer} can be subclassed instead of using this class.
- * 
+ *
  * @param <T>
  *            The enum type managed by this serializer.
  * @author <a href="mailto:joffrey.bion@gmail.com">Joffrey BION</a>
@@ -15,10 +15,10 @@ public class EnumSerializer<T extends Enum<T>> extends SimpleSerializer<T> {
 
     /**
      * Creates a new {@link EnumSerializer} for the specified enum class.
-     * 
+     *
      * @param clazz
-     *            A class that extends {@link Enum}, and which has to be consistent
-     *            with the type variable.
+     *            A class that extends {@link Enum}, and which has to be consistent with the type
+     *            variable.
      */
     public EnumSerializer(Class<T> clazz) {
         super(clazz);
